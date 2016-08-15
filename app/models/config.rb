14351697076a2827ccb99572f61f7d0c859ecea7
Armando Fox <fox@cs.berkeley.edu>
2016-08-14 17:51:51 -0700

    attribute encryption and Figaro-ize secret key

--- app/models/config.rb
+++ app/models/config.rb
@@ -2,9 +2,10 @@ class Config < ActiveRecord::Base
   belongs_to :project
   scope :for, ->(metric_name) { where(:metric_name => metric_name).first }
   validates_uniqueness_of :metric_name, :scope => :project_id
-  serialize :options, Hash
 
-  attr_encrypted :options, :key => Figaro.env.attr_encrypted_key!
+  attr_encrypted :options, :key => Figaro.env.attr_encrypted_key!, :marshal => true
+  after_initialize { self.options ||= {} }
+
   attr_accessible :options, :metric_name
 
 end
--- db/migrate/20160803182348_encrypt_config_options_and_raw_sample_data.rb
+++ db/migrate/20160803182348_encrypt_config_options_and_raw_sample_data.rb
@@ -1,6 +1,8 @@
 class EncryptConfigOptionsAndRawSampleData < ActiveRecord::Migration
   def change
     rename_column :configs, :options, :encrypted_options
+    add_column    :configs, :encrypted_options_iv, :string
     rename_column :metric_samples, :raw_data, :encrypted_raw_data
+    add_column    :metric_samples, :encrypted_raw_data_iv, :string
   end
 end
