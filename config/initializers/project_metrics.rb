ec64d97c05f61500ad5f96c6b06885bbe98d4ce7
Sam Joseph <tansaku@gmail.com>
2016-08-22 17:09:07 +0100

    adds more metrics and allows individual metric update

--- config/initializers/project_metrics.rb
+++ config/initializers/project_metrics.rb
@@ -2,4 +2,6 @@ ProjectMetrics.configure do
   add_metric :project_metric_code_climate
   add_metric :project_metric_github
   add_metric :project_metric_slack
+  add_metric :project_metric_pivotal_tracker
+  add_metric :project_metric_slack_trends
 end
